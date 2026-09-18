9fc36a266fb56b34cf727eab60f48d39a2014614
hiennhxai <hiennhx.ai@gmail.com>
2026-09-18 23:09:06 +0700

    Thêm GTM

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -148,8 +148,19 @@ export default async function RootLayout({
 
         {/* ==========================================
             MARKETING TRACKING SCRIPTS
-            Google Analytics 4
+            Google Tag Manager & Google Analytics 4
         ========================================== */}
+        {/* Google Tag Manager */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-NFLM4L48');`
+          }}
+        />
+
         {/* Google Analytics */}
         <script async src="https://www.googletagmanager.com/gtag/js?id=G-PWN56EC56G"></script>
         <script dangerouslySetInnerHTML={{
@@ -178,6 +189,15 @@ export default async function RootLayout({
         }} />
       </head>
       <body suppressHydrationWarning>
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe 
+            src="https://www.googletagmanager.com/ns.html?id=GTM-NFLM4L48"
+            height="0" 
+            width="0" 
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
         {/* Facebook Pixel (NoScript) */}
         <noscript>
           <img height="1" width="1" style={{ display: "none" }}
